b651546e12cbc6422af84843a98871c8fdd9eb1e
Lewis Lovett <llovett7575@gmail.com>
2022-09-29 16:40:05 +0100

    Created handleNumberBtnClick and attchEvents js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,3 +2,16 @@ let currentTotalSum;
 let modifier;
 let currentNumber;
 
+const handleNumberBtnClick = (event) => {
+    currentNumber = event.target.value;
+    console.log(currentNumber);
+}
+
+
+const attachEvents = () => {
+    numberButtons = document.querySelectorAll(".numButton");
+    numberButtons.forEach(numButton => {
+        addEventListener("click", handleNumberBtnClick);
+    });
+}
+attachEvents();
