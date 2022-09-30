45788dd65db6c16c65fcaf96a43876f5582d80d8
Lewis Lovett <llovett7575@gmail.com>
2022-09-30 10:22:10 +0100

    Updated handleNumbers to handle multiple digits

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,8 +3,14 @@ let modifier;
 let currentNumber;
 
 const handleNumberBtnClick = (event) => {
-    currentNumber = event.target.value;
-    displayInputs(currentNumber);
+    if(currentNumber==0){
+        currentNumber = Number(event.target.value);
+    }else{
+        currentNumber = Number(`${currentNumber}${event.target.value}`);
+    }
+    
+    console.log(currentNumber);
+    displayInputs(event.target.value);
 }
 
 const handleModifierBtnClick = (event) => {
@@ -15,6 +21,7 @@ const handleModifierBtnClick = (event) => {
 const displayInputs = (input) =>{
     document.querySelector(".calcContainer__numInfo--input").value += input;
 }
+
 const clearCalculator = () =>{
     document.querySelector(".calcContainer__numInfo--input").value = "";
     document.querySelector(".calcContainer__numInfo--output").value = 0;
@@ -22,6 +29,8 @@ const clearCalculator = () =>{
     modifier = "";
     currentNumber = 0;
 }
+
+
 
 const attachEvents = () => {
     const numberButtons = document.querySelectorAll(".numButton");
