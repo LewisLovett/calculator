e28557c0315f9d0c80583966b198fdcf401039e9
Lewis Lovett <llovett7575@gmail.com>
2022-10-03 12:38:22 +0100

    Added pi button with functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,7 +5,7 @@ let currentNumber = 0;
 let pastFirstNumber = false; //Checks if the user has entered a second number -- used in calculate() function
 let prevInputModifier = false; // Checks if prev input was a modifier to prevent multiple modifiers being input
 let firstNumberInputted = false; //Checks if the first number has been inputted so the user cannot enter a modifer as the first input;
-let decimalSelected = false;
+let decimalSelected = false; //Checks if the user has selected the decimal button
 let decimalNumberToAdd = "0.";
 
 
@@ -23,6 +23,14 @@ const handleNumberBtnClick = (event) => {
     }
     displayInputs(event.target.value);
     prevInputModifier = false;}
+}
+
+//Function to handle pi button click. Same as number button exept it set currentNumber to pi
+const handlePiBtnClick = (event) => {
+    firstNumberInputted = true; 
+    currentNumber = Math.PI;
+    displayInputs("pi");
+    prevInputModifier = false;
 }
 
 //Function for when the user click a modifier button. If statement checks if the previous input is a modifier. 
@@ -115,21 +123,25 @@ const handleClearBtnClick = () =>{
 //Handle +/- button. Takes the current number, removes it from the input html, multiples it by -1 to turn to either positive or negative
 //then adds the new value to the input html.
 const handlePosNegBtnClick = () =>{
-    let htmlInput = document.querySelector(".calcContainer__numInfo--input").value;
-    let lengthOfCurrentNum = currentNumber.toString().length;
-    document.querySelector(".calcContainer__numInfo--input").value = htmlInput.slice(0,-lengthOfCurrentNum );
-    currentNumber *= -1;
-    document.querySelector(".calcContainer__numInfo--input").value += currentNumber;
+    if(firstNumberInputted && currentNumber!=0){
+        let htmlInput = document.querySelector(".calcContainer__numInfo--input").value;
+        let lengthOfCurrentNum = currentNumber.toString().length;
+        document.querySelector(".calcContainer__numInfo--input").value = htmlInput.slice(0,-lengthOfCurrentNum );
+        currentNumber *= -1;
+        document.querySelector(".calcContainer__numInfo--input").value += currentNumber;
+    }
 }
 
 //Handle percentage button. Removes current number from input html. Takes the current number and divides it by 100
 // and displays the percentage in the input html.
 const handlePercentButton = () => {
-    let lengthOfCurrentNumber = String(currentNumber).length;
-    let htmlInput = document.querySelector(".calcContainer__numInfo--input").value;
-    document.querySelector(".calcContainer__numInfo--input").value = htmlInput.slice(0,-lengthOfCurrentNumber);
-    currentNumber /= 100;
-    document.querySelector(".calcContainer__numInfo--input").value += currentNumber;
+    if(firstNumberInputted && currentNumber!=0){
+        let lengthOfCurrentNumber = String(currentNumber).length;
+        let htmlInput = document.querySelector(".calcContainer__numInfo--input").value;
+        document.querySelector(".calcContainer__numInfo--input").value = htmlInput.slice(0,-lengthOfCurrentNumber);
+        currentNumber /= 100;
+        document.querySelector(".calcContainer__numInfo--input").value += currentNumber;
+    }
 }
 
 //Handle decimal button. Changes a boolan variable to true so now when the user inputs a number it will be considered a decimal addition
@@ -157,6 +169,7 @@ const attachEvents = () => {
     document.querySelector(".percentButton").addEventListener("click", handlePercentButton);
     document.querySelector(".decimalButton").addEventListener("click", handleDecimalButton);
     document.querySelector(".posNegButton").addEventListener("click", handlePosNegBtnClick);
+    document.querySelector(".piButton").addEventListener("click", handlePiBtnClick);
 }
 
 attachEvents();
